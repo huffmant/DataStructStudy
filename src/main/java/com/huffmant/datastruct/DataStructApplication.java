6e857c1faf2251002b59a06dd437e4d15b217e5c
huffman tang <huffmantang@huffmandeAir.lan>
2021-09-19 07:23:16 +0800

    first commit

---
+++ src/main/java/com/huffmant/datastruct/DataStructApplication.java
@@ -0,0 +1,13 @@
+package com.huffmant.datastruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huffmant/datastruct/DataStructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huffmant.datastruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
